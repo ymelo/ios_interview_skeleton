7c2b1893da7ea0773a0509f32f3d45a98c28470e
Yohann Melo <ymelo@intellimec.com>
2020-02-13 16:48:28 -0500

    More mocked data, easier for the candidate to read

--- InterviewStoryboard/MockedData.swift
+++ InterviewStoryboard/MockedData.swift
@@ -5,28 +5,51 @@
 //  Created by Yohann Melo on 2020-02-13.
 //  Copyright © 2020 Yohann Melo. All rights reserved.
 //
-
+/*
+This class is a very simple example to give you an idea of what the data should look like.
+It's on purpose using dictionary so we didn't have to worry about the actual structures to store them.
+You can edit this class as much as you want to fit your needs!
+*/
 import Foundation
 class MockedData {
     
-    fileprivate func json(fromDictionary dictionary: [String : String]) -> String? {
+    /// Simple helper method to generate some JSON strings
+    /// The strings generated still contain some escaping characters, you may need to edit this method :)
+    fileprivate func json(fromDictionary dictionary: [String : Any]) -> String? {
         do {
-            let json = try JSONSerialization.data(withJSONObject: dictionary, options: [])
-            return String(data: json, encoding: .ascii)
+            let json = try JSONSerialization.data(withJSONObject: dictionary, options: [.withoutEscapingSlashes])
+            return String(data: json, encoding: .utf8)
         } catch {
             print(error.localizedDescription)
         }
         return nil
     }
+    
+    /// Retreive the "current user" (consider this to be your user when you log-in
+    /// The strings generated still contain some escaping characters, you may need to edit this method :)
     func currentUser() -> String? {
         return user(withId: 2)
     }
     
+    /// Retreive a user
+    /// The strings generated still contain some escaping characters, you may need to edit this method :)
     func user(withId id: Int) -> String? {
         switch id {
         case 0: return json(fromDictionary: ["id": "0", "name": "Someone else", "profilePicture": "url_here"])
         case 1: return json(fromDictionary: ["id": "1", "name": "Someone else 1", "profilePicture": "url_here"])
         case 2: return json(fromDictionary: ["id": "2", "name": "Me", "profilePicture": "url_here"])
+        default:
+            return nil
+        }
+    }
+    
+    /// Retreives the list of meetings
+    /// The strings generated still contain some escaping characters, you may need to edit this method :)
+    func meetings(forUser user: User) -> String? {
+        switch user.id {
+        case 2:
+            
+            return "[{\"name\":\"Meeting 1\", \"participants\":[{\"id\": \"0\", \"name\": \"Someone\", \"profilePicture\": \"url_here\"}, {\"id\": \"1\", \"name\": \"Someone else\", \"profilePicture\": \"url_here\"}]}]"
         default:
             return nil
         }
