6115d1fd894086b632f0418723d29363b34a4ba9
Yohann Melo <ymelo@intellimec.com>
2020-02-13 16:50:58 -0500

    Simple Cell

---
+++ InterviewStoryboard/SimpleCollectionCell.swift
@@ -0,0 +1,15 @@
+//
+//  SimpleCollectionCell.swift
+//  InterviewStoryboard
+//
+//  Created by Yohann Melo on 2020-02-13.
+//  Copyright © 2020 Yohann Melo. All rights reserved.
+//
+
+import Foundation
+import Foundation
+import UIKit
+class SimpleCollectionCell: UICollectionViewCell {
+    @IBOutlet weak var title: UILabel!
+}
+
